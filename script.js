965ce9ed8b7b58e79d98e68af84e84ccd1d8f5cd
fqkvxq <takuyakawai32@gmail.com>
2020-01-28 01:43:31 +0900

    initial

---
+++ script.js
@@ -0,0 +1,25 @@
+new ClipboardJS(".copyButton");
+var firstPointText = "";
+var reasonText = "";
+var exampleText = "";
+var secondPointText = "";
+
+
+// フォームに文字入力をしたときの挙動
+function inputText() {
+    firstPointText = document.getElementById('firstPointText').value;
+    reasonText = document.getElementById('reasonText').value;
+    exampleText = document.getElementById('exampleText').value;
+    secondPointText = document.getElementById('secondPointText').value;
+    document.getElementById('generatedTextArea').value = firstPointText + reasonText + exampleText + secondPointText;
+}
+
+function deleteInputs() {
+    if (confirm("削除しますか？")) {
+        document.getElementById('firstPointText').value = "";
+        document.getElementById('reasonText').value = "";
+        document.getElementById('exampleText').value = "";
+        document.getElementById('secondPointText').value = "";
+        document.getElementById('generatedTextArea').value = "";
+    }
+}
